5c1bfe38901689c61edfd88a7125f0f2fe3344a5
Krystian Walus <Krystian.Walus@mail.bcu.ac.uk>
2023-02-05 11:56:38 +0000

    Player changed to Player2

--- RRE/Assets/Scripts/Player/Collision.cs
+++ RRE/Assets/Scripts/Player/Collision.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    public GameObject Player2;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         this.GetComponent<Movement>().cloud = false;
         transform.position -= new Vector3(0, 1, 0);
         //turn into tree
+        Instantiate(Player2, this.transform.position, Quaternion.identity);
+        Destroy(this);
     }
 }
